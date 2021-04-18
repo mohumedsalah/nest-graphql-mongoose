1018db2a84dccc23681d7d21d96ff5650a47886b
salah <mohumed.s.omar@gmail.com>
2021-04-18 04:41:57 +0200

    small refactor

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { authConstants } from './constants';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './guard/jwt.strategy';
 
 @Module({
   imports: [
